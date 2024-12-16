2d6ae8104f9c5a8bbf23cf1e0dd23740fb0a41fb
Hadraha <Hadraha@NatClassRoom1.training.dom>
2024-12-16 13:32:24 +0200

    a

--- examples/async/useAsyncAwait/index.js
+++ examples/async/useAsyncAwait/index.js
@@ -2,13 +2,11 @@
 const axios = require("axios")
 
 const countriesUrl = "https://restcountries.com/v3.1/all"
+const countriesUrlLocal = "http://localhost:2200/countries-elbit"
 
 async function getCountriesData(){
     try {
-            const result = await axios.get(countriesUrl)
-            const result2 = await fetch(countriesUrl)
-            const countries = await result2.json()
-            console.log(countries[128].name.common)
+            const result = await axios.get(countriesUrlLocal)
             console.log(result.data[128].name.common)
     } catch (error) {
             console.log(error.message)
