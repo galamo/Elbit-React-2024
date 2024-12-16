afb1dea64696f06b999e9e6f144b26089a2a932f
Hadraha <Hadraha@NatClassRoom1.training.dom>
2024-12-16 15:15:03 +0200

    a

---
+++ examples/typescript/src/index.types.ts
@@ -0,0 +1,37 @@
+enum Companies {
+  IBM = 1,
+  Elbit,
+  Google,
+  Facebook,
+}
+
+type User = {
+  userName: string;
+  company: Companies;
+};
+
+const user1: User = {
+  userName: "galamouyal88@gmail.com",
+  company: Companies.IBM,
+};
+const user2: User = { userName: "eli@gmail.com", company: Companies.Elbit };
+
+type Company = {
+  location: CompanyLocation;
+  name: string;
+  id: number;
+  cfo: string;
+};
+
+type CompanyLocation = {
+  city: string;
+  address: string;
+  street: number;
+};
+
+const company: number = {
+  name: "IBM",
+  id: 124,
+  cfo: "Naor",
+  location: { city: "Givatayim", address: "arieal sharon", street: 4 },
+};
