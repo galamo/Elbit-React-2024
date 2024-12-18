64d38317c484ce4a03adb8830269bc40ed4b5f76
Hadraha <Hadraha@NatClassRoom1.training.dom>
2024-12-18 14:11:04 +0200

    a

--- examples/typescript/src/generics/2.ts
+++ examples/typescript/src/generics/2.ts
@@ -25,38 +25,32 @@ const users: User[] = [
     { type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut' }
 ];
 
-export type ApiResponse<T> = unknown;
+export type ApiResponse<T> = {
+    status: 'error';
+    error: string;
+} | {
+    status: 'success';
+    data: T[];
+};
 
-type AdminsApiResponse = (
-    {
-        status: 'success';
-        data: Admin[];
-    } |
-    {
-        status: 'error';
-        error: string;
-    }
-);
 
-export function requestAdmins(callback: (response: AdminsApiResponse) : void) {
+export function requestAdmins(callback: (response: ApiResponse<Admin>) : void) {
     callback({
         status: 'success',
         data: admins
     });
 }
 
-type UsersApiResponse = (
-    {
-        status: 'success';
-        data: User[];
-    } |
-    {
-        status: 'error';
-        error: string;
-    }
-);
+// export function requestSomething<U>(callback: (response: ApiResponse<U>)) {
+//     callback({
+//         status: 'success',
+//         data: []
+//     });
+// }
 
-export function requestUsers(callback: (response: UsersApiResponse) : void) {
+
+
+export function requestUsers(callback: (response: ApiResponse<User>) : void) {
     callback({
         status: 'success',
         data: users
