7881026355ab703992da07bfa417e7fe99b962a5
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2024-12-22 08:19:46 +0200

    style

--- react-client-application/src/App.tsx
+++ react-client-application/src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { ButtonAppsExamples } from "./components/ui/button";
 import Header from "./components/ui/header";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
       <Header title={"App header"} color={"red"} />
       <Header title={"Statistics Header"} color={"green"} />
       <Header title={"About"} color={"BLUE"} />
+      <ButtonAppsExamples />
     </>
   );
 }
