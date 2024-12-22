0eb8a748a6afcb3013a999f795d5f40f2b02ca2a
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2024-12-22 07:34:29 +0200

    routing

--- react-client-application/src/App.tsx
+++ react-client-application/src/App.tsx
@@ -1,14 +1,74 @@
+import { Outlet, Route, Routes, RouteProps, Link } from "react-router";
 import "./App.css";
 import Header from "./components/ui/header";
+
+const routesApp: RouteProps[] = [
+  { path: "/", element: <Home /> },
+  { path: "/about", element: <About /> },
+  { path: "*", element: <NotFound /> },
+];
 
 function App() {
   return (
     <>
       <Header title={"App header"} color={"red"} />
-      <Header title={"Statistics Header"} color={"green"} />
-      <Header title={"About"} color={"BLUE"} />
+      <Link to="/">home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/countries/search">countries</Link>
+      <Routes>
+        {routesApp.map((r: RouteProps) => {
+          return <Route {...r} />;
+        })}
+        <Route path="countries" element={<Countries />}>
+          {/* <Route element={<Countries />} /> */}
+          <Route index element={<Search />} />
+          <Route path=":stats" element={<Stats />} />
+        </Route>
+      </Routes>
+      <Outlet />
     </>
   );
+}
+
+function Home() {
+  return <div> Home</div>;
+}
+
+function About() {
+  return <div> About</div>;
+}
+
+function Countries() {
+  return (
+    <div>
+      <h1>Countries</h1>
+      <div>
+        <Link to="/countries/search">search</Link>
+      </div>
+      <div>
+        <Link to="/countries/param1">go to about</Link>
+      </div>
+      {/* will either be <Home/> or <Settings/> */}
+      <Outlet />
+    </div>
+  );
+}
+
+function Stats() {
+  return <div> Stats</div>;
+}
+function Search() {
+  return (
+    <div>
+      <h2> Search component </h2>
+      <div>
+        <Link to="/about">go to about</Link>
+      </div>
+    </div>
+  );
+}
+function NotFound() {
+  return <h1> NOT FOUND</h1>;
 }
 
 export default App;
