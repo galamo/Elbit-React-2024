3db48eb59bb1841329f4189a3c82076950b9427b
Hadraha <Hadraha@NatClassRoom1.training.dom>
2024-12-30 11:08:23 +0200

    a

--- react-client-application/src/components/header/index.tsx
+++ react-client-application/src/components/header/index.tsx
@@ -1,12 +1,44 @@
 import { NavLink } from "react-router";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 import css from "./style.module.css";
+
 export default function HeaderNavigation() {
   return (
-    <div className={css.marginNavs}>
-      <NavLink to={"/login"}> Login </NavLink>
-      <NavLink to={"/register"}> Register </NavLink>
-      <NavLink to={"/countries"}> countries </NavLink>
-      <NavLink to={"/countries-reports"}> reports </NavLink>
-    </div>
+    <Box sx={{ flexGrow: 1 }} className={css.marginNavs}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">
+            <NavLink to={"/login"}> Login </NavLink>
+          </Button>
+          <Button color="inherit">
+            <NavLink to={"/register"}> Register </NavLink>
+          </Button>
+          <Button color="inherit">
+            <NavLink to={"/countries"}> countries </NavLink>
+          </Button>
+          <Button color="inherit">
+            <NavLink to={"/countries-reports"}> reports </NavLink>
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
