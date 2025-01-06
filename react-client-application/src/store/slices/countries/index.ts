11c22c80e7c82e083a202e84bb27beaab4c24915
Hadraha <Hadraha@NatClassRoom1.training.dom>
2025-01-06 16:05:29 +0200

    thunk

--- react-client-application/src/store/slices/countries/index.ts
+++ react-client-application/src/store/slices/countries/index.ts
@@ -1,52 +1,67 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { CountryApi } from "../../../components/pages/countriesPage";
+import axios from "axios";
 
 interface ICountriesState {
-  countries: Array<CountryApi>;
-  favorites: Array<CountryApi>;
-  counter: number;
+    countries: Array<CountryApi>;
+    favorites: Array<CountryApi>;
+    counter: number;
+    isLoading: boolean
+    errorMessage: string
 }
 
 const initState: ICountriesState = {
-  countries: [],
-  favorites: [],
-  counter: 0,
+    countries: [],
+    favorites: [],
+    counter: 0,
+    isLoading: false,
+    errorMessage: ""
 };
 
 export const countriesSlice = createSlice({
-  name: "countries",
-  initialState: initState,
-  reducers: {
-    addToFavorite: (state, action: PayloadAction<CountryApi>) => {
-      state.favorites.push(action.payload);
+    name: "countries",
+    initialState: initState,
+    reducers: {
+        addToFavorite: (state, action: PayloadAction<CountryApi>) => {
+            state.favorites.push(action.payload);
+        },
+        increaseCounter: (state, action: PayloadAction<number>) => {
+            state.counter = state.counter + 1;
+        },
     },
-    increaseCounter: (state, action: PayloadAction<number>) => {
-      state.counter = state.counter + 1;
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchCountries.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(
+                fetchCountries.fulfilled,
+                (state, action: PayloadAction<Array<CountryApi>>) => {
+                    state.countries = action.payload
+                    state.isLoading = false;
+                }
+            )
+            .addCase(
+                fetchCountries.rejected,
+                (state) => {
+                    state.countries = [];
+                    state.isLoading = false;
+                    state.errorMessage = "Something went wrong"
+                }
+            );
     },
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(fetchCountries.pending, (state) => {})
-      .addCase(
-        fetchCountries.fulfilled,
-        (state, action: PayloadAction<CountryApi>) => {}
-      )
-      .addCase(
-        fetchCountries.rejected,
-        (state, action: PayloadAction<unknown>) => {}
-      );
-  },
 });
-
+const URL_ALL = "http://localhost:2200/api/countries-delay";
 export const fetchCountries = createAsyncThunk("fetch/countries", async () => {
-  try {
-    const response = await axios.get("INSERT URL");
-
-    return response.data;
-  } catch (error) {
-    return (error as Error).message;
-  }
+    try {
+        const result = await axios.get(URL_ALL);
+        const { data } = result;
+        return data;
+    } catch (error) {
+        return (error as Error).message;
+    }
 });
+
 
 export const { addToFavorite, increaseCounter } = countriesSlice.actions;
 export default countriesSlice.reducer;
