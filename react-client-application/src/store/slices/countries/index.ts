7c5c353a9057a4425f320034a4cc7edeb7c17912
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2025-01-06 15:52:48 +0200

    a

--- react-client-application/src/store/slices/countries/index.ts
+++ react-client-application/src/store/slices/countries/index.ts
@@ -1,31 +1,52 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit"
-import { CountryApi } from "../../../components/pages/countriesPage"
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { CountryApi } from "../../../components/pages/countriesPage";
 
 interface ICountriesState {
-    countries: Array<CountryApi>;
-    favorites: Array<CountryApi>;
-    counter: number
+  countries: Array<CountryApi>;
+  favorites: Array<CountryApi>;
+  counter: number;
 }
 
 const initState: ICountriesState = {
-    countries: [],
-    favorites: [],
-    counter: 0
-}
+  countries: [],
+  favorites: [],
+  counter: 0,
+};
 
 export const countriesSlice = createSlice({
-    name: "countries",
-    initialState: initState,
-    reducers: {
+  name: "countries",
+  initialState: initState,
+  reducers: {
+    addToFavorite: (state, action: PayloadAction<CountryApi>) => {
+      state.favorites.push(action.payload);
+    },
+    increaseCounter: (state, action: PayloadAction<number>) => {
+      state.counter = state.counter + 1;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCountries.pending, (state) => {})
+      .addCase(
+        fetchCountries.fulfilled,
+        (state, action: PayloadAction<CountryApi>) => {}
+      )
+      .addCase(
+        fetchCountries.rejected,
+        (state, action: PayloadAction<unknown>) => {}
+      );
+  },
+});
 
-        addToFavorite: (state, action: PayloadAction<CountryApi>) => {
-            state.favorites.push(action.payload)
-        },
-        increaseCounter: (state, action: PayloadAction<number>) => {
-            state.counter = state.counter + 1
-        }
-    }
-})
+export const fetchCountries = createAsyncThunk("fetch/countries", async () => {
+  try {
+    const response = await axios.get("INSERT URL");
+
+    return response.data;
+  } catch (error) {
+    return (error as Error).message;
+  }
+});
 
 export const { addToFavorite, increaseCounter } = countriesSlice.actions;
-export default countriesSlice.reducer
+export default countriesSlice.reducer;
